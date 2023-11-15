fd2f6ce8ed35d0bf0662a5006f6c7eb8d64914f6
wierza <dawid@wierzycki.pl>
2023-11-15 16:16:59 +0100

    Add sharing client folder and add path

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const socket = require('socket.io');
+const path = require('path');
 
 const app = express();
 
 let tasks =[];
+
+app.use(express.static(path.join(__dirname, '/client')))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/index.html'));
+});
 
 const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running...');
